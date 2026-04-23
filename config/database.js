2f7e0035de2ae66c51f1643deb382682f1a9bcc8
= <=>
2026-04-23 20:05:11 +0530

    Database Conifg

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/url_shortener";
+  process.env.MONGODB_URI;
 
 async function connectDatabase() {
   await mongoose.connect(MONGODB_URI);
